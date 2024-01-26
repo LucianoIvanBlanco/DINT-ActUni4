cd6fe1f7000258012a0a1ccac09da916673ed5e0
Luciano Ivan Blanco <luciano.blanco@live.u-tad.com>
2024-01-26 08:57:41 -0800

    Initial Commit

---
+++ ActUni4/ActUni4App.swift
@@ -0,0 +1,17 @@
+//
+//  ActUni4App.swift
+//  ActUni4
+//
+//  Created by user248205 on 26/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ActUni4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActUni4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ActUni4
+//
+//  Created by user248205 on 26/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
